fae75ff8cb45660c0f76801d44b175db8fbddd90
Ernesto-Alvarez <ernesto.alvarez@gmail.com>
2017-08-22 22:05:52 -0300

    Reduced update frequency and fixed loop to use the frequency parameter.

--- pigv2/backends/glue.py
+++ pigv2/backends/glue.py
@@ -22,7 +22,7 @@ class hub(object):
 #Network range gate: takes an IP packet from the input queue and passes it to the output queue if and only if the IP source is within a list of dymanically changing networks.
 #Takes an input function, an output function and an update function (which returns a list of addresses, usually database.ip_network_table.ip_list)
 class network_range_gate(object):
-	def __init__(self,input,output,update,update_frequency=0.1):
+	def __init__(self,input,output,update,update_frequency=0.5):
 		self.input = input;
 		self.output = output;
 		self.addresses = []
@@ -62,7 +62,7 @@ class network_range_gate(object):
 
 	def update_addresses(self):
 		while True:
-			time.sleep(0.1) # to avoid a busy loop
+			time.sleep(self.update_frequency) # to avoid a busy loop
 			self.db_semaphore.acquire()
 			self.addresses = self.update_function()
 			self.db_semaphore.release()
