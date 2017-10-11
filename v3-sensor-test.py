d1571d181926d577de59d8ab7f47d356dfa93eb7
Ernesto-Alvarez <ernesto.alvarez@gmail.com>
2017-10-11 01:25:39 -0300

    Capture/Record/Replay test

--- v3-sensor-test.py
+++ v3-sensor-test.py
@@ -1,14 +1,33 @@
-from pigv3.sensors import live_sensor
-from pigv3.sensors import pcap_recorder
-from pigv3.sensors import replay_sensor
+from pigv3.packet_mgmt import live_sensor
+from pigv3.packet_mgmt import pcap_recorder
+from pigv3.packet_mgmt import replay_sensor
 from Queue import Queue
 import time
 
-
 q = Queue()
-#s = live_sensor(q.put,"wlp3s0")
-#r = pcap_recorder(q.get,"/tmp/x.pcap")
-p = replay_sensor(q.put,"/tmp/x.pcap",timing_multiplier=100)
+q2 = Queue()
+q3 = Queue()
+s = live_sensor(q.put,"wlp3s0")
+r = pcap_recorder(q2.get,"/tmp/x.pcap")
 
-time.sleep(20)
+print "=================LIVE======================"
 
+packet = q.get()
+ts_stop = time.time() + 5
+while packet['type'] != 'Capture End':
+	q2.put(packet)
+	print packet
+	if time.time() > ts_stop:
+		packet = { 'type' : 'Capture End' }
+	else:
+		packet = q.get()
+r.kill()
+
+print "==================REPLAY===================="
+
+
+p = replay_sensor(q3.put,"/tmp/x.pcap",replay_speed=1,repetitions=1)
+packet = q3.get()
+while packet['type'] != 'Capture End':
+	print packet
+	packet = q3.get()
